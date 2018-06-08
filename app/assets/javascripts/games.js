6a1b7fd9af229e0577d123998c605a0c1543fcaf
Michael Bach <migobach@gmail.com>
2018-06-08 11:07:52 -0600

    add controllers

---
+++ app/assets/javascripts/games.js
@@ -0,0 +1,21 @@
+var currentGame = {};
+
+$(document).ready( function() {
+  $('.game-item').on('click', function() {
+    currentGame.id = this.dataset.id
+    currentGame.name = this.dataset.name
+    $.ajax({
+      url: '/games/' + currentGame.id + '/characters',
+      method: 'GET',
+      datatype: 'JSON'
+    }).done( function(characters) {
+      $('#game').text('Characters in' + currentGame.name)
+      var list = $('#characters');
+      list.empty();
+      characters.forEach( function(char) {
+        var li = '<li data-character-id="' + char.id + '">' + char.name + '-' + char.power + '</li>'
+        list.append(li)
+      })
+    })
+  })
+})
